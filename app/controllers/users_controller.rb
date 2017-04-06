1d9a221b187ee16829b62245ad97c7c3ecae460a
drewstpierce <bicandvlood@gmail.com>
2017-04-05 22:32:11 -0400

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,24 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "And so begins your delve into the demi-realm of lexical chronosynclastia."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get '/login', to: 'static_pages#login'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',    to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 end
